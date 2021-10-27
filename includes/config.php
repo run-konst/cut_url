7a6de938d83956f8d66f405735b3dc5c05383daa
Руньковский Константин <raitok@mail.ru>
2021-10-27 21:05:20 +0300

    lesson1

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+    define('SITE_NAME', 'Cut your URL');
+    define('HOST', 'http://' . $_SERVER['HTTP_HOST']);
+
+    const DB_HOST = 'http://cut-your-url';
+    const DB_NAME = 'cut_url';
+    const DB_USER = 'root';
+    const DB_PASS = '';
+    
